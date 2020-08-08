2ec2944e690502aee0ca45a6f00c423c810565cb
Yolibeth Sánchez <yolibeth.diana@gmail.com>
2020-08-08 10:56:41 -0600

    [ADD]: Organización completa del módulo Proyectos.

--- DyCODE-Frontend/dycode-angular/src/app/app-routing.module.ts
+++ DyCODE-Frontend/dycode-angular/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
+import { TodosComponent } from './proyectos/todos/todos.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppAccesoComponent } from './app-acceso/app-acceso.component';
 import { RegistroComponent } from './acceso/registro/registro.component';
 import { LoginComponent } from './acceso/login/login.component';
 import { LandingComponent } from './landing/landing.component';
+import { AppProyectosComponent } from './app-proyectos/app-proyectos.component';
 
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
-  { path: 'acceso', component: AppAccesoComponent, children: [{ path:'login', component: LoginComponent }, { path: 'registro', component: RegistroComponent }] }
+  { path: 'acceso', component: AppAccesoComponent, children: [{ path:'login', component: LoginComponent }, { path: 'registro', component: RegistroComponent }] },
+  { path: 'dycode', component: AppProyectosComponent, children: [{path:'', component: TodosComponent }]}
 ];
 
 @NgModule({
