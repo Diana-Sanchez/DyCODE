5ed854521734c988290831e196e90cda38930024
Yolibeth Sánchez <yolibeth.diana@gmail.com>
2020-08-08 06:34:15 -0600

    [ADD]: Landin angular y componentes para el acceso a la plataforma.

--- DyCODE-Frontend/dycode-angular/src/app/app.module.ts
+++ DyCODE-Frontend/dycode-angular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './acceso/inicio/inicio.component';
+import { NavbarComponent } from './acceso/navbar/navbar.component';
+import { FooterComponent } from './acceso/footer/footer.component';
+import { LoginComponent } from './acceso/login/login.component';
+import { RegistroComponent } from './acceso/registro/registro.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    RegistroComponent,
+  
   ],
   imports: [
     BrowserModule,
