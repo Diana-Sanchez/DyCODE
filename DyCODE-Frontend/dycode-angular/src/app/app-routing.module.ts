04e633872afc82fe90f41d6a1a4360736bddc9fb
Yolibeth Sánchez <yolibeth.diana@gmail.com>
2020-08-09 18:10:06 -0600

    [ADD]: Componente para proyectos destacados.

--- DyCODE-Frontend/dycode-angular/src/app/app-routing.module.ts
+++ DyCODE-Frontend/dycode-angular/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ComponentFixture } from '@angular/core/testing';
 import { TodosComponent } from './proyectos/todos/todos.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -7,12 +8,13 @@ import { LoginComponent } from './acceso/login/login.component';
 import { LandingComponent } from './landing/landing.component';
 import { AppProyectosComponent } from './app-proyectos/app-proyectos.component';
 import { InicioComponent } from './proyectos/inicio/inicio.component';
+import { DestacadosComponent } from './proyectos/destacados/destacados.component';
 
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'acceso', component: AppAccesoComponent, children: [{ path:'login', component: LoginComponent }, { path: 'registro', component: RegistroComponent }] },
-  { path: 'dycode', component: AppProyectosComponent, children: [{path:'', component: InicioComponent }]}
+  { path: 'proyectos', component: AppProyectosComponent, children: [{path:'', component: InicioComponent }, { path:'destacados', component: DestacadosComponent}, { path:'todos', component: InicioComponent}]}
 ];
 
 @NgModule({
