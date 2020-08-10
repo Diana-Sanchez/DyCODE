101f391569f6336aa749f2769762c05992186457
Yolibeth Sánchez <yolibeth.diana@gmail.com>
2020-08-10 13:05:18 -0600

    [ADD]: Diseño edición de componentes.

--- DyCODE-Frontend/dycode-angular/src/app/app-routing.module.ts
+++ DyCODE-Frontend/dycode-angular/src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture } from '@angular/core/testing';
-import { TodosComponent } from './proyectos/todos/todos.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppAccesoComponent } from './app-acceso/app-acceso.component';
@@ -11,12 +10,21 @@ import { InicioComponent } from './proyectos/inicio/inicio.component';
 import { DestacadosComponent } from './proyectos/destacados/destacados.component';
 import { PapeleraComponent } from './proyectos/papelera/papelera.component';
 import { RecientesComponent } from './proyectos/recientes/recientes.component';
+import { TodosComponent } from './proyectos/todos/todos.component';
 
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
-  { path: 'acceso', component: AppAccesoComponent, children: [{ path:'login', component: LoginComponent }, { path: 'registro', component: RegistroComponent }] },
-  { path: 'proyectos', component: AppProyectosComponent, children: [{path:'', component: InicioComponent }, { path:'destacados', component: DestacadosComponent}, { path:'todos', component: InicioComponent}, {path:'papelera', component: PapeleraComponent}, { path:'recientes', component: RecientesComponent}]}
+  { path: 'acceso', component: AppAccesoComponent, children: [
+    { path:'login', component: LoginComponent }, 
+    { path: 'registro', component: RegistroComponent }] },
+  { path: 'proyectos', component: AppProyectosComponent, children: [
+    { path:'', component: InicioComponent},
+    { path:'destacados', component: DestacadosComponent}, 
+    { path:'todos', component: InicioComponent}, 
+    { path:'papelera', component: PapeleraComponent}, 
+    { path:'recientes', component: RecientesComponent},
+    { path:'edicion', component: TodosComponent}]}
 ];
 
 @NgModule({
--- DyCODE-Frontend/dycode-angular/src/app/proyectos/todos/todos.component.ts
+++ DyCODE-Frontend/dycode-angular/src/app/proyectos/todos/todos.component.ts
@@ -7,18 +7,28 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
   @ViewChild('editor') editor;
-    text: string = "";
- 
+  @ViewChild('editor2') editor2;
+  @ViewChild('editor3') editor3;
+
     ngAfterViewInit() {
-        this.editor.setTheme("dracula");
-        this.editor.setMode("javascript")
- 
+        this.editor.setTheme("asphalt");
+        this.editor.setMode("html")
         this.editor.getEditor().setOptions({
             enableBasicAutocompletion: true
         });
- 
-        
+        this.editor2.setTheme("asphalt");
+        this.editor2.setMode("css")
+        this.editor2.getEditor().setOptions({
+            enableBasicAutocompletion: true
+        }); 
+        this.editor3.setTheme("asphalt");
+        this.editor3.setMode("javascript")
+        this.editor3.getEditor().setOptions({
+            enableBasicAutocompletion: true
+        }); 
+       
     }
+    
   
   constructor() { }
 
