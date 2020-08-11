8c9b1c935e3822e07930a8d82fffda4420184917
Yolibeth Sánchez <yolibeth.diana@gmail.com>
2020-08-10 22:15:08 -0600

    [ADD]: Estructura de la base de datos.

---
+++ DyCODE-Backend/estructura.js
@@ -0,0 +1,155 @@
+usuarios = [
+    {
+        primerNombre: 'Diana', 
+        primerApellido:'Sánchez',
+        correo: 'diana@gmail.com',
+        password:'12345',
+        proyectos: [
+            {
+                nombreProyecto:'Proyecto 1',
+                descripcionProyecto:'Este es mi primer proyecto'
+            },
+            {
+                nombreProyecto:'Proyecto 2',
+                descripcionProyecto:'Proyecto de la clase de Industria del software'
+            },
+            {
+                nombreProyecto:'Proyecto 3',
+                descripcionProyecto:'Proyecto de prueba'
+            },
+            {
+                nombreProyecto:'Proyecto 4',
+                descripcionProyecto:'Chambaline, segundo proyecto de la clase de Industria'
+            },
+            {
+                nombreProyecto:'Proyecto 5',
+                descripcionProyecto:'Este es mi quinto proyecto'
+            },
+            {
+                nombreProyecto:'Proyecto 6',
+                descripcionProyecto:'Proyecto de carácter urgente'
+            },
+        ],
+        snippets: [
+            {
+                nombre:'snippet 1',
+                tipo: 'PHP',
+            },
+            {
+                nombre:'snippet 2',
+                tipo: 'PHP',
+            },
+            {
+                nombre:'snippet 3',
+                tipo: 'HTML',
+            }
+        ]
+    },
+    {
+        primerNombre: 'Diana', 
+        primerApellido:'Sánchez',
+        correo: 'diana@gmail.com',
+        password:'12345',
+        proyectos: [
+            {
+                nombreProyecto:'Proyecto 1',
+                descripcionProyecto:'Este es mi primer proyecto'
+            },
+            {
+                nombreProyecto:'Proyecto 2',
+                descripcionProyecto:'Proyecto de la clase de Industria del software'
+            },
+            {
+                nombreProyecto:'Proyecto 3',
+                descripcionProyecto:'Proyecto de prueba'
+            },
+            {
+                nombreProyecto:'Proyecto 4',
+                descripcionProyecto:'Chambaline, segundo proyecto de la clase de Industria'
+            },
+            {
+                nombreProyecto:'Proyecto 5',
+                descripcionProyecto:'Este es mi quinto proyecto'
+            },
+            {
+                nombreProyecto:'Proyecto 6',
+                descripcionProyecto:'Proyecto de carácter urgente'
+            },
+        ],
+        snippets: [
+            {
+                nombre:'snippet 1',
+                tipo: 'PHP',
+            },
+            {
+                nombre:'snippet 2',
+                tipo: 'PHP',
+            },
+            {
+                nombre:'snippet 3',
+                tipo: 'HTML',
+            }
+        ]
+    },
+    {
+        primerNombre: 'Diana', 
+        primerApellido:'Sánchez',
+        correo: 'diana@gmail.com',
+        password:'12345',
+        proyectos: [
+            {
+                nombreProyecto:'Proyecto 1',
+                descripcionProyecto:'Este es mi primer proyecto'
+            },
+            {
+                nombreProyecto:'Proyecto 2',
+                descripcionProyecto:'Proyecto de la clase de Industria del software'
+            },
+            {
+                nombreProyecto:'Proyecto 3',
+                descripcionProyecto:'Proyecto de prueba'
+            },
+            {
+                nombreProyecto:'Proyecto 4',
+                descripcionProyecto:'Chambaline, segundo proyecto de la clase de Industria'
+            },
+            {
+                nombreProyecto:'Proyecto 5',
+                descripcionProyecto:'Este es mi quinto proyecto'
+            },
+            {
+                nombreProyecto:'Proyecto 6',
+                descripcionProyecto:'Proyecto de carácter urgente'
+            },
+        ],
+        snippets: [
+            {
+                nombre:'snippet 1',
+                tipo: 'PHP',
+            },
+            {
+                nombre:'snippet 2',
+                tipo: 'PHP',
+            },
+            {
+                nombre:'snippet 3',
+                tipo: 'HTML',
+            }
+        ]
+    },
+]
+
+planes = [
+    {
+        nombre:'DyCODE FREE',
+        descripcion:'Plan sin costo'
+    },
+    {
+        nombre:'DyCODE PREMIUM',
+        descripcion:'Plan sin costo'
+    },
+    {
+        nombre:'DyCODE GOLDEN',
+        descripcion:'Plan sin costo'
+    },
+]
